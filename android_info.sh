448339bce2d019f890b278bafdea5670a1a24187
jhonaAM554 <TU_CORREO_DE_GITHUB>
2026-09-20 23:16:02 -0500

    Agregar informacion de CPU y memoria RAM

--- android_info.sh
+++ android_info.sh
@@ -17,3 +17,19 @@ echo "Modelo              : $modelo"
 echo "Android             : $version_android"
 echo "Arquitectura        : $arquitectura"
 echo "Kernel              : $kernel"
+
+echo ""
+echo "[ HARDWARE ]"
+echo ""
+
+nucleos=$(nproc)
+
+echo "Nucleos de CPU      : $nucleos"
+
+ram=$(free -h | awk '/^Mem:/ {print $2}')
+ram_usada=$(free -h | awk '/^Mem:/ {print $3}')
+ram_disponible=$(free -h | awk '/^Mem:/ {print $7}')
+
+echo "RAM total           : $ram"
+echo "RAM utilizada       : $ram_usada"
+echo "RAM disponible      : $ram_disponible"
